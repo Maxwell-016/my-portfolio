e76b58625f3df33af84b4953fed9f85a9072e5b8
Maxwell-016 <ndungumaxwell057@gmail.com>
2025-02-05 17:27:58 +0300

    Added caching using react's useQuery

---
+++ src/App.jsx
@@ -0,0 +1,122 @@
+
+import workingAnimation from "./assets/whatIdo.png";
+import webDev from "./assets/webDev.png";
+import mobileDev from "./assets/appDev.png";
+import firebase from "./assets/firebase.png";
+import NavBar from "./components/navBar";
+import CircularAvatar from "./components/circularAvatar";
+import IntroText from "./components/introText";
+import SimpleDescription from "./components/simpleDesc";
+import Socials from "./components/socials";
+import BouncingArrow from "./components/bouncingArrow";
+import Title from "./components/title";
+import AnimatedCard from "./components/animatedCard";
+import AboutMeText from "./components/aboutMEText";
+import ServiceCard from "./components/serviceCard";
+import ContactForm from "./components/contactForm";
+import Footer from "./components/footer";
+import ThemeSwitcher from "./components/themeSwitcher";
+import { FaBars} from "react-icons/fa";
+
+import { useContent } from "./hooks/useContent";
+import "./App.css";
+function App() {
+  const {content, isLoading, error} = useContent();
+  console.log(content);
+  
+
+  //Hero section
+  const altText = "My profile Photo";
+  const imageUrl = isLoading? "Loading..." :content.hero?.imageUrl;
+  const title = isLoading? "Loading..." :content.hero?.title;
+  const name = isLoading? "Loading..." :content.hero?.name;
+  const continuation = isLoading? "Loading..." :content.hero?.continuation;
+  const description = isLoading? "Loading..." :content.hero?.description
+
+  //About me section
+  const aboutMeDescription = isLoading? "Loading..." :content.about?.description;
+  const phone = isLoading? "Loading..." :content.about?.phone;
+  const email = isLoading? "Loading..." :content.about?.email;
+  const language = isLoading? "Loading..." :content.about?.language;
+  const freelance = isLoading? "Loading..." :content.about?.freelance;
+
+  
+  //Services section
+    const webDevDescription =isLoading? "Loading..." :content.services?.webDevDescription;
+    const mobileDevDescription =isLoading? "Loading..." :content.services?.mobileDevDescription;
+    const backendDevDescription =isLoading? "Loading..." :content.services?.backendDevDescription;
+   
+  //Contact section
+  const getInTouch = isLoading? "Loading..." :content.contact?.getInTouch;
+
+  return (
+    <>
+      <div className="first-page" id="home">
+        <input type="checkbox" name="sideNav" id="sideNav" />
+        <label htmlFor="sideNav" className="bars">
+          <FaBars></FaBars>
+        </label>
+        <div className="side-bar">
+          <div className="top-navigation">
+            <div></div>
+            <NavBar />
+            <ThemeSwitcher />
+          </div>
+        </div>
+        <div className="small-screen-theme">
+          <ThemeSwitcher />
+        </div>
+
+
+        <div className="introduction">
+          <CircularAvatar imageUrl={imageUrl} altText={altText} />
+          <IntroText title={title} name={name} continuation={continuation} />
+          <SimpleDescription description={description} />
+          <Socials />
+          <BouncingArrow />
+        </div>
+      </div>
+
+
+      <div className="other-page">
+        <Title title="About Me" id="about" />
+        <div className="about-me">
+          <AnimatedCard image={workingAnimation} altText="working animation" />
+          <AboutMeText description= {aboutMeDescription} name = {name} phone = {phone} email = {email} language = {language} freelance = {freelance}/>
+        </div>
+
+        <Title title="Services" id="services" />
+        <div className="services">
+          <div className="service-cards">
+            <ServiceCard
+              imageUrl={webDev}
+              altText="Web development"
+              description={webDevDescription}
+              title="Web Development"
+            />
+            <ServiceCard
+              imageUrl={mobileDev}
+              altText="Mobile development"
+              description={mobileDevDescription}
+              title="App Development"
+            />
+            <ServiceCard
+              imageUrl={firebase}
+              altText="backend development"
+              description={backendDevDescription}
+              title="Back-end Development"
+            />
+          </div>
+        </div>
+
+        <Title title="Contact Me" id="contact" />
+        <div className="contact">
+          <ContactForm  getInTouch = {getInTouch}/>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
--- src/hooks/useContent.js
+++ src/hooks/useContent.js
@@ -1,33 +1,31 @@
-import { useEffect, useState } from "react";
+import {useQuery} from "@tanstack/react-query";
 import { collection, getDocs } from "firebase/firestore";
-import { db } from "../firebase";
+import { db } from "../firebase"
 
+const fetchContent = async () => {
+  const querySnapshot = await getDocs(collection(db, "reactPortfolio"));
+  const contentData = {};
+  querySnapshot.forEach((doc) => {
+    contentData[doc.id] = doc.data();
+  });
+  return contentData;
+};
+
+//Caching using useQuery
 const useContent = () => {
-  const [content, setContent] = useState({});
-  const [isLoading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const fetchContent = async () => {
-      try {
-        const querySnapshot = await getDocs(collection(db, "reactPortfolio"));
-        const contentData = {};
-        querySnapshot.forEach((doc) => {
-          contentData[doc.id] = doc.data();
-        });
-        setContent(contentData);
-        setLoading(false);
-      } catch (e) {
-        setError(e);
-        setLoading(false);
-      }
-    };
-    fetchContent();
-  }, []);
-  return {
-    content,
+  const {
+    data: content,
     isLoading,
     error,
-  };
+  } = useQuery({
+    queryKey: ["reactPortfolio"],
+    queryFn: fetchContent,
+    refetchOnWindowFocus: false,
+    refetchOnMount: false,
+    refetchOnReconnect: false,
+    staleTime: 1000 * 60 * 10,
+    cacheTime: 1000 * 60 * 30,
+  });
+  return {content, isLoading, error};
 };
 export { useContent };
